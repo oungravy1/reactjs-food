2e64c280acc0c821233ca53bc4a541b1cd5240f9
oungravy1 <oungravy1@gmail.com>
2023-03-12 15:19:19 +0700

    prepare a good more on structure project and also have some components

---
+++ projectfood/src/components/app/index.tsx
@@ -0,0 +1,25 @@
+import React from"react";
+import "./index.css";
+import DefaultLayout from "./layout/DefaultLayout";
+
+ 
+const App: React.FC = () => {
+  return (
+  
+  <div>
+      <DefaultLayout>
+
+        hello kok
+
+        <h1>Hi</h1>
+        
+        <h2>Bye</h2>
+
+      </DefaultLayout>
+   </div>
+   
+  )
+   
+}
+
+export default App;
