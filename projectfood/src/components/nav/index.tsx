a0de90ccf2232a56b03b48b994c64f34f6d36d71
oungravy1 <oungravy1@gmail.com>
2023-03-12 21:19:00 +0700

    update menu from tranditional to dynamic menu in projectfood

--- projectfood/src/components/nav/index.tsx
+++ projectfood/src/components/nav/index.tsx
@@ -3,17 +3,52 @@ import "./index.css";
 
 interface NavProps{}
 
+const menubar = [
+    {
+        link: "#about",
+        label: "About",
+    },
+    {
+        link: "#menu",
+        label: "Menu",
+    },
+    {
+        link: "#contact",
+        label: "Contact",
+    },
+    
+
+];
+
 const Nav: React.FC<NavProps> = (props) => {
     const {} = props;
+
+    console.log("HELLO MENU BAR: ", menubar)
 
     return (
 
         <nav>
-                <a href="#" className="logo">PROJECT FOOD</a>
+                <a href="#" className="logo">Project Food</a>
+
+                    {/* <ul>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#menu">Menu</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>  */}
                     <ul>
-                        <li><a href="#">ABOUT</a></li>
-                        <li><a href="#">MENU</a></li>
-                        <li><a href="#">CONTACT</a></li>
+                        {
+                            menubar.map((item, index) => (
+                                <li key={index}>
+                                    <a href={item.link}>{item.label}</a>
+                                </li>
+                            ))
+                        }
                     </ul> 
         </nav>
 
